638378b7cc84833803dd50e2fb7bbd5beafd8559
Lee Jia Yu <lee_jia_yu@s2019.ssts.edu.sg>
2020-09-12 11:47:19 +0800

    Initial Commit

---
+++ CM Project 2020/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CM Project 2020
+//
+//  Created by Jia Yu Lee on 12/9/20.
+//  Copyright © 2020 Jia Yu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
